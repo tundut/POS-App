305f7e21bea12899db2b50153a7f3fb31354776a
tundut <ducnguyenphan05@gmail.com>
2026-05-19 19:15:58 +0700

    Feat: connect db to aws rds

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const { Pool } = require('pg');
 require('dotenv').config();
 
-const connectionString = process.env.DATABASE_URL || process.env.DATABASE_URL_LOCAL || 'postgresql://postgres:postgres@localhost:5432/pos_db';
+const connectionString = process.env.DATABASE_URL || process.env.DATABASE_URL_LOCAL;
 
 const pool = new Pool({
   connectionString,
